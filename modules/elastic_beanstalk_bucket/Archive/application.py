a840768e5d56c15409b4e0c18c8c36f8b489ae8a
daniyal_Lodhi <daniyal@eurustechnologies.com>
2020-10-26 22:28:32 +0500

    Latest time added

--- modules/elastic_beanstalk_bucket/Archive/application.py
+++ modules/elastic_beanstalk_bucket/Archive/application.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return ' MOnday 26 October 2020 9:51 PM'
+    return ' MOnday 26 October 2020 10:25 PM'
     # return 'Hello World! I have been hit)
 
 if __name__ == "__main__":
